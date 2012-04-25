77667ea11f902261dfe900a3f465d23260be9dff
Hexor Chang <hexor@hexor-VPCEB19FX.(none)>
2012-04-25 11:52:31 +0800

    add the project just created

---
+++ src/org/hexor/oa/SmartOAActivity.java
@@ -0,0 +1,15 @@
+package org.hexor.oa;
+
+import org.hexor.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmartOAActivity extends Activity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
